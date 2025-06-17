c2c8d82993d0fa684a6499b3334e8e1ddc192686
summer-want-sushi <summershao00@gmail.com>
2025-06-17 09:24:48 -0400

    Add Wikipedia lookup support to GAIAAgent

--- agents/gaia_agent.py
+++ agents/gaia_agent.py
@@ -1,5 +1,6 @@
 from tools.reverse_string_tool import reverse_string
 from tools.filter_vegetables_tool import filter_vegetables
+from tools.wikipedia_lookup_tool import lookup_wikipedia
 
 class GAIAAgent:
     def __init__(self):
@@ -16,6 +17,17 @@ class GAIAAgent:
         if "vegetable" in lower_q or "veggie" in lower_q:
             print("GAIAAgent using filter_vegetables tool.")
             return filter_vegetables(question)
+
+        wiki_keywords = [
+            "wikipedia",
+            "nominated",
+            "studio albums",
+            "first name",
+            "featured article",
+        ]
+        if any(keyword in lower_q for keyword in wiki_keywords):
+            print("GAIAAgent using lookup_wikipedia tool.")
+            return lookup_wikipedia(question)
         fixed_answer = "This is a default answer."
         print(f"Agent returning fixed answer: {fixed_answer}")
         return fixed_answer
